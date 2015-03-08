b97c7e0234e5781ea3b22f18f35cafb6904c3368
jharia <jharia@users.noreply.github.com>
2015-03-08 15:52:52 -0700

    Update script.js
    scroll to top of menu on 'top' click

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
         || location.hostname == this.hostname) {
 
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        // var target = $(this.hash);
+        // target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           var target = $('.thumbnail');
            if (target.length) {
              $('html,body').animate({
                  scrollTop: target.offset().top
